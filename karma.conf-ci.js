c733690bdbf030375cca44ed122c0803a0f49988
shiwenzhe <shiwenzhe@baidu.com>
2015-06-23 18:06:50 +0800

    add browser shim

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -123,6 +123,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'test/typedarray.js',
+      'test/polyfill.js',
       'build/jsbuf.js',
       'test/Long.js',
       'test/ByteBufferAB.js',
--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'test/polyfill.js',
       'test/typedarray.js',
       'build/jsbuf.js',
       'test/Long.js',
